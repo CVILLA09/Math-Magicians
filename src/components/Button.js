a2a2bca95c5d250eb5648378bf0eac7b23024841
Cristian Villa <c.villavirgen@gmail.com>
2023-08-16 16:41:55 -0600

    Erase unnecessary button.css

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Button.css';
+import './Calculator.css';
 
 const Button = ({ label }) => {
   // Determine the class name based on the label value
   let buttonClass = 'button';
   if (label === '0') buttonClass += ' button-0';
-  if (['÷', 'X', '-', '='].includes(label)) buttonClass += ' button-orange';
+  if (['÷', 'x', '+', '-', '='].includes(label)) buttonClass += ' button-orange';
 
   return (
     <button type="button" className={buttonClass}>
