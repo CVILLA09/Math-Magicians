76448be1e54bd96202ee4c3e4360862ffeb165c2
Cristian Villa <c.villavirgen@gmail.com>
2023-08-16 15:11:43 -0600

    Create Display.js file for calculator display

---
+++ src/components/Display.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Display = () => (
+  <div style={{ width: '4ratios', height: '1ratio', border: '1px solid black', padding: '10px' }}>
+    {/* Content goes here */}
+  </div>
+);
+
+export default Display;
