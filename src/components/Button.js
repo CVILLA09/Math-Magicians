f901e1b53eb3804f2ff186e920f99592e1388544
Cristian Villa <c.villavirgen@gmail.com>
2023-08-16 16:20:11 -0600

    Install prop-types, fix code style, and add button type attribute

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 import './Button.css';
 
 const Button = ({ label }) => {
-    // Determine the class name based on the label value
-    let buttonClass = 'button';
-    if (label === '0') buttonClass += ' button-0';
-    if (['÷', 'X', '-', '='].includes(label)) buttonClass += ' button-orange';
-  
-    return (
-      <button className={buttonClass}>
-        {label}
-      </button>
-    );
-  };  
+  // Determine the class name based on the label value
+  let buttonClass = 'button';
+  if (label === '0') buttonClass += ' button-0';
+  if (['÷', 'X', '-', '='].includes(label)) buttonClass += ' button-orange';
+
+  return (
+    <button type="button" className={buttonClass}>
+      {label}
+    </button>
+  );
+};
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
