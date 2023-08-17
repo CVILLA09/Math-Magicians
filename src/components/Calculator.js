5a45d38f8e439463a52e10ff76cd3f38d03aa226
Cristian Villa <c.villavirgen@gmail.com>
2023-08-16 20:45:25 -0600

    Add state to Calculator component for tracking calculations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from './logic/calculate';
 import Display from './Display';
 import Button from './Button';
 import './Calculator.css';
 
-const Calculator = () => (
-  <div className="calculator">
-    <Display />
-    <div className="buttons">
-      <Button label="AC" />
-      <Button label="+/-" />
-      <Button label="%" />
-      <Button label="÷" />
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Button label="x" />
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Button label="-" />
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Button label="+" />
-      <Button label="0" />
-      <Button label="." />
-      <Button label="=" />
+const Calculator = () => {
+  // Declare the state inside the component
+  const [calculatorData, setCalculatorData] = useState({ total: null, next: null, operation: null });
+
+  return (
+    <div className="calculator">
+      <Display />
+      <div className="buttons">
+        <Button label="AC" />
+        <Button label="+/-" />
+        <Button label="%" />
+        <Button label="÷" />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="x" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
