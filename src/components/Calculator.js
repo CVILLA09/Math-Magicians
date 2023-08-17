6b94a14902a452f5a94b6ab0fc5e9bb116bf0f57
Cristian Villa <c.villavirgen@gmail.com>
2023-08-16 20:59:54 -0600

    Add onClick event handlers to calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,30 +12,30 @@ const Calculator = () => {
     const newCalculatorData = calculate(calculatorData, buttonName);
     setCalculatorData(newCalculatorData);
   };
-  
+
   return (
     <div className="calculator">
       <Display />
       <div className="buttons">
-        <Button label="AC" />
-        <Button label="+/-" />
-        <Button label="%" />
-        <Button label="÷" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="x" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" />
-        <Button label="0" />
-        <Button label="." />
-        <Button label="=" />
+        <Button label="AC" onClick={() => handleButtonClick('AC')} />
+        <Button label="+/-" onClick={() => handleButtonClick('+/-')} />
+        <Button label="%" onClick={() => handleButtonClick('%')} />
+        <Button label="÷" onClick={() => handleButtonClick('÷')} />
+        <Button label="7" onClick={() => handleButtonClick('7')} />
+        <Button label="8" onClick={() => handleButtonClick('8')} />
+        <Button label="9" onClick={() => handleButtonClick('9')} />
+        <Button label="x" onClick={() => handleButtonClick('x')} />
+        <Button label="4" onClick={() => handleButtonClick('4')} />
+        <Button label="5" onClick={() => handleButtonClick('5')} />
+        <Button label="6" onClick={() => handleButtonClick('6')} />
+        <Button label="-" onClick={() => handleButtonClick('-')} />
+        <Button label="1" onClick={() => handleButtonClick('1')} />
+        <Button label="2" onClick={() => handleButtonClick('2')} />
+        <Button label="3" onClick={() => handleButtonClick('3')} />
+        <Button label="+" onClick={() => handleButtonClick('+')} />
+        <Button label="0" onClick={() => handleButtonClick('0')} />
+        <Button label="." onClick={() => handleButtonClick('.')} />
+        <Button label="=" onClick={() => handleButtonClick('=')} />
       </div>
     </div>
   );
