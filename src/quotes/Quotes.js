5c588f690d09365a53a47503397b3b1b4fc4f057
Cristian Villa <c.villavirgen@gmail.com>
2023-08-18 16:57:52 -0600

    Implement quote fetching from API and handle loading and error states

--- src/quotes/Quotes.js
+++ src/quotes/Quotes.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Quotes() {
-  return (
-    <div>
-      {/* Quote will be displayed here */}
-    </div>
-  );
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.api-ninjas.com/v1/quotes?category=dreams', {
+      headers: { 'X-Api-Key': 'CswAFd5+15BCNhWkX0QvMQ==2j0HO1d24mVADbcM' },
+    })
+      .then((response) => {
+        setQuote(response.data[0]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  let content;
+
+  if (loading) {
+    content = <p>Loading...</p>;
+  } else if (error) {
+    content = (
+      <p>
+        Error:
+        {' '}
+        {error.message}
+      </p>
+    );
+  } else {
+    content = <p>{quote ? quote.quote : 'No quote available'}</p>;
+  }
+
+  return <div>{content}</div>;
 }
 
 export default Quotes;
