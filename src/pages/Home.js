4c157844c7542a34a3b35ddcd1d35bd8e8cf93e5
Cristian Villa <c.villavirgen@gmail.com>
2023-08-22 18:17:28 -0600

    Create Home.js file for Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to our page</h1>
+    <p>Lorem ipsum dolor sit amet, consecteur...</p>
+    <p>Lorem ipsum dolor sit amet, consecteur...</p>
+  </div>
+);
+
+export default Home;
