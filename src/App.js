3f18d44885577cc05eff1221ec483c057eaea8ac
Cristian Villa <c.villavirgen@gmail.com>
2023-08-22 18:43:08 -0600

    Set up React Router to manage navigation between pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './layout/Navbar';
 import Footer from './layout/Footer';
 import Home from './pages/Home';
 import Calculator from './components/Calculator';
-import Quote from './quotes/Quotes';
-
-
+import Quotes from './quotes/Quotes';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <Quotes />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/calculator" component={Calculator} />
+          <Route path="/quotes" component={Quotes} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
