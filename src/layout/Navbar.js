15665b18eb9614849dccc7215f06c4137e45b098
Cristian Villa <c.villavirgen@gmail.com>
2023-08-22 18:22:17 -0600

    Create the Navbar Component

---
+++ src/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => (
+  <nav>
+    <div>Math Magicians</div>
+    <div>
+      <a href="/">Home</a> | <a href="/calculator">Calculator</a> | <a href="/quote">Quote</a>
+    </div>
+  </nav>
+);
+
+export default Navbar;
