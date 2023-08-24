ed8aff937ebdf0eeec38c0d1ab0784d8aa7c5647
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-08-24 16:24:31 -0400

    Add tests for buttons in the calculate function

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -6,8 +6,34 @@ let testObj = {
   operation: null,
 }
 
+let testObj2 = {
+  total: 100,
+  next: 50,
+  operation: 'x',
+}
+
+let testObj3 = {
+  total: "5000",
+  next: null,
+  operation: null,
+}
+
+let testObj4 = {
+  total: "-5000",
+  next: null,
+  operation: null,
+}
+
 describe('Testing calculate function', () => {
-  test('Test button', () => {
+  test('Test AC button', () => {
     expect(calculate(testObj, 'AC')).toEqual(testObj);
+  });
+
+  test('Test = button', () => {
+    expect(calculate(testObj2, '=')).toEqual(testObj3);
+  });
+
+  test('Test +/- button', () => {
+    expect(calculate(testObj3, '+/-')).toEqual(testObj4);
   });
 });
